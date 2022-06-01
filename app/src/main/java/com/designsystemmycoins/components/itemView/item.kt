e05c87693f538599926723cbef99a242c10eb492
lesley.ferreira <lesley.ferreira@thoughtworks.com>
2022-06-01 14:57:30 -0300

    add components item view

---
+++ app/src/main/java/com/designsystemmycoins/components/itemView/item.kt
@@ -0,0 +1,73 @@
+package com.designsystemmycoins.components.itemView
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.designsystemmycoins.data.ItemData
+
+@Composable
+fun Item(
+    item: ItemData
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(
+                    id = item.imageRes
+                ),
+                contentDescription = "item - image cell",
+                modifier = Modifier
+                    .padding(4.dp, 4.dp, 0.dp, 4.dp)
+                    .clip(CircleShape)
+                    .size(50.dp)
+
+            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+            ){
+                Text(text = item.title)
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ){
+                    Text(text = item.value.toString(), fontSize = 12.sp, fontWeight = FontWeight.Light)
+                    Text(text = item.year.toString(), fontSize = 12.sp, fontWeight = FontWeight.Light)
+                    Text(text = item.conservationState, fontSize = 12.sp, fontWeight = FontWeight.Light, modifier = Modifier.padding(0.dp, 0.dp, 4.dp, 0.dp))
+                }
+            }
+            Icon(
+                imageVector = Icons.Filled.Menu,
+                contentDescription = "item - menu icon",
+                modifier = Modifier.padding(0.dp, 0.dp, 4.dp, 0.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ItemPreview(){
+    val itemData = ItemData()
+    Item(itemData)
+}
---
+++ app/src/main/java/com/designsystemmycoins/components/itemView/itemDatails.kt
@@ -0,0 +1,69 @@
+package com.designsystemmycoins.components.itemView
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.designsystemmycoins.data.DetailsData
+import com.designsystemmycoins.ui.theme.backgroundGreen
+import com.designsystemmycoins.ui.theme.backgroundGrey
+import com.designsystemmycoins.ui.theme.textColorGrey
+import com.example.designsystemmycoins.R
+
+@Composable
+fun Details(titleRes: Int, description: DetailsData){
+
+    Column (
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.background(backgroundGrey)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(20.dp)
+                .background(backgroundGreen)
+                .padding(0.dp, 0.dp, 0.dp, 4.dp)
+
+        ) {
+            Text(
+                text = stringResource(id = titleRes),
+                modifier = Modifier.align(Alignment.Center),
+                color = Color.White,
+            )
+        }
+
+        description.getDetail().forEach { description ->
+            Text(
+                text = description.key + " : " + description.value.toString(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(20.dp)
+                    .padding(4.dp, 0.dp, 0.dp, 4.dp),
+                color = textColorGrey,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailsPreview(){
+    val detailsData = DetailsData()
+
+    Details(
+        titleRes = R.string.item_details_title,
+        description = detailsData
+    )
+}
